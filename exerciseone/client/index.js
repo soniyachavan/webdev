064ace79c1280e659f808c061a9348023fb7f720
hafizali05 <hafizdidarali@gmail.com>
2015-05-06 18:00:26 +0530

    complete project

--- exerciseone/client/index.js
+++ exerciseone/client/index.js
@@ -43,7 +43,7 @@
 			
 			// var text=event.target.text.value;
 			// console.log('text',text);
-			var name= template.find("input[name=name]");
+			var name= template.find("#expense_name");
 			console.log('name value:', name.value);
 
 			var amount = template.find("input[name=amount]");
@@ -76,7 +76,13 @@
 
 	Template.allmyexpense.helpers({
 		expenses: function() {
-			return Expenses.find().fetch();
+			// username= Session.get('username');
+			if(Session.get('selectedUser')) {
+				return Expenses.find({ user_id: Session.get('selectedUser') }).fetch();
+			}
+			else {
+				return Expenses.find().fetch();
+			}
 		},
 		uniqueUsers: function(){
 			var expenses = Expenses.find().fetch();
@@ -106,9 +112,40 @@
 	// })
 
 Template.dropdownUsers.helpers({
-	// username: function() {
-	// 	console.log('dropdownusers: ',this);
-	// 	return this.username;
-	// 	// return Meteor.users.findOne(this).username;
-	// }	
+	user : function() {
+		user=Expenses.find().fetch();
+		console.log('user:', user);
+	},
+	uniqueUsers: function(){
+			var expenses = Expenses.find().fetch();
+			var user_ids = _.pluck(expenses, 'user_id');
+			
+			user_ids = _.uniq(user_ids);
+			console.log(user_ids);
+			var users = Meteor.users.find({ _id: { $in: user_ids } }).fetch();
+			console.log(users);
+			return users;
+		}
+});
+
+// Template.dropdownusers.events({
+//     'click .player': function(){
+//         Session.set('selectedUser', 'session value test');
+//         Session.get('selectedUser');
+
+//     },
+Template.dropdownUsers.events({
+	// 'change select#username' : function() {
+	// 	user = $('#username option:selected').attr('name');
+	// 	console.log('user value:', user);
+	// 	if(user)
+	// 		Session.set('username', user);
+	// }
+	'click .unique-user' : function () {
+		// user_id = Meteor.user
+		console.log(this._id);
+		Session.set('selectedUser', this._id)
+	}
 })
+	
+// });
